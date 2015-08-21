9980807cf7592a8acbabc17e8993051445be5c93
Liam Wooding <liamwooding@gmail.com>
2015-08-21 15:27:44 +0100

    Added signin page

--- client/client.js
+++ client/client.js
@@ -1,7 +1,20 @@
 FlowLayout.setRoot('#content')
 
+AccountsTemplates.configure({
+  onSubmitHook: function (err) {
+    if (err) return console.error(err)
+    FlowRouter.go('/')
+  }
+})
+
 Template.body.onRendered(function () {
 
+})
+
+Template.body.events({
+  'click #at-nav-button': function () {
+    FlowRouter.go('/sign-in')
+  }
 })
 
 Template.registerHelper('isReady', function (sub) {
@@ -13,4 +26,7 @@ Template.registerHelper('isReady', function (sub) {
 })
 Template.registerHelper('isEq', function (a, b) {
   return a === b
+})
+Template.registerHelper('user', function () {
+  return Meteor.user()
 })
